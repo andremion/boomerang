dc153090c44d05c765be9d1986d4350df2c81e14
André Mion <andremion@gmail.com>
2024-03-21 15:54:06 +0000

    Fix the version of the generated Package.swift file

--- sample/shared/build.gradle.kts
+++ sample/shared/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kmmbridge)
@@ -26,13 +28,32 @@ kotlin {
     }
 }
 
+val GROUP: String by project
+val AUTO_VERSION = project.property(
+    if (project.hasProperty("AUTO_VERSION")) {
+        "AUTO_VERSION"
+    } else {
+        "XCFRAMEWORK_VERSION"
+    }
+) as String
+
+group = GROUP
+version = AUTO_VERSION
+
+val BUILD_TYPE = if (project.hasProperty("ENABLE_PUBLISHING")) {
+    NativeBuildType.RELEASE
+} else {
+    NativeBuildType.DEBUG
+}
+
+kmmbridge {
+    buildType.set(BUILD_TYPE)
+    mavenPublishArtifacts()
+    spm()
+}
+
 // Set up in your ~/.gradle/gradle.properties file
 // GITHUB_REPO=your_repo
 // GITHUB_PUBLISH_USER=your_username
 // GITHUB_PUBLISH_TOKEN=your_token
 addGithubPackagesRepository()
-
-kmmbridge {
-    mavenPublishArtifacts()
-    spm()
-}
